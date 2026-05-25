356417c038c79f6f17d296e297ed8ac550f94b51
Tony Stark <vbhat2006@gmail.com>
2026-05-25 10:57:51 +0530

    finished expense entity class and added foreign key

--- src/main/java/com/example/Expense_Tracker/Expense.java
+++ src/main/java/com/example/Expense_Tracker/Expense.java
@@ -1,7 +1,16 @@
 package com.example.Expense_Tracker;
 
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 
@@ -10,5 +19,77 @@ import jakarta.persistence.Table;
 @Table(name="expenses")
 public class Expense 
 {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="exp_id")
+    private Long expId; //primary key
+
+    private Double amount;
+
+    private String description;
+
+    private String category;
+
+    @Column(name="expense_date")
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable=false)
+    private User user; // foreign key
+
+    //setters and getters
+
+    public Long getExpId()
+    {
+        return expId;
+    }
+    public void setExpId(Long expId)
+    {
+        this.expId=expId;
+    }
+
+    public Double getAmount()
+    {
+        return amount;
+    }
+    public void setAmount(Double amount)
+    {
+        this.amount=amount;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+    public void setDescription(String description)
+    {
+        this.description=description;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+    public void setCategory(String category)
+    {
+        this.category=category;
+    }
+
+    public LocalDate getDate()
+    {
+        return date;
+    }
+    public void setDate(LocalDate date)
+    {
+        this.date=date;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+    public void setUser(User user)
+    {
+        this.user=user;
+    }
 }
