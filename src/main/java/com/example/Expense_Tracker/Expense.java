b0c96dc3460d8d8cd741561b0497668972b007c2
Tony Stark <vbhat2006@gmail.com>
2026-05-25 10:18:10 +0530

    made expense entity class and dashboard

---
+++ src/main/java/com/example/Expense_Tracker/Expense.java
@@ -0,0 +1,14 @@
+package com.example.Expense_Tracker;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name="expenses")
+public class Expense 
+{
+    
+}
